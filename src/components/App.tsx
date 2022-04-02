2755a86a25f156dfc9f456ba0bd89d0e9791a945
Rakesh Baruah <rbaruah@gmail.com>
2022-04-02 08:53:35 -0500

    add cart class component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import pizzas from "../data/pizzas.json";
 import Pizza from "./Pizza";
+import Cart from "./Cart";
 import AppCSS from "./App.module.css";
 
 const App = () => {
   return (
     <div className={AppCSS.container}>
+      <div className={AppCSS.header}>
+        <div className={AppCSS.siteTitle}>Delicious Pizza</div>
+        <Cart />
+      </div>
       <ul>
         {pizzas.map((pizza) => (
           <Pizza key={pizza.id} pizza={pizza} />
