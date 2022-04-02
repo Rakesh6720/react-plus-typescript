b955333ec677920cea9446a530ed6a0011dc6fbd
Rakesh Baruah <rbaruah@gmail.com>
2022-04-02 11:35:55 -0500

    add context and useState

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,20 +3,23 @@ import pizzas from "../data/pizzas.json";
 import Pizza from "./Pizza";
 import Cart from "./Cart";
 import AppCSS from "./App.module.css";
+import AppStateProvider from "./AppState";
 
 const App = () => {
   return (
-    <div className={AppCSS.container}>
-      <div className={AppCSS.header}>
-        <div className={AppCSS.siteTitle}>Delicious Pizza</div>
-        <Cart />
+    <AppStateProvider>
+      <div className={AppCSS.container}>
+        <div className={AppCSS.header}>
+          <div className={AppCSS.siteTitle}>Delicious Pizza</div>
+          <Cart />
+        </div>
+        <ul>
+          {pizzas.map((pizza) => (
+            <Pizza key={pizza.id} pizza={pizza} />
+          ))}
+        </ul>
       </div>
-      <ul>
-        {pizzas.map((pizza) => (
-          <Pizza key={pizza.id} pizza={pizza} />
-        ))}
-      </ul>
-    </div>
+    </AppStateProvider>
   );
 };
 
--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import CartCSS from "./Cart.module.css";
 import { FiShoppingCart } from "react-icons/fi";
+import { AppStateContext } from "./AppState";
 
 interface Props {}
 
@@ -14,31 +15,43 @@ class Cart extends React.Component<Props, State> {
     this.state = {
       isOpen: false,
     };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    this.setState((prevState) => ({ isOpen: !prevState.isOpen }));
   }
 
   render() {
     return (
-      <div className={CartCSS.cartContainer}>
-        <button
-          type="button"
-          className={CartCSS.button}
-          onClick={() => {
-            this.setState((prevState) => ({ isOpen: !prevState.isOpen }));
-          }}
-        >
-          <FiShoppingCart />
-          <span>2 pizza(s)</span>
-        </button>
-        <div
-          className={CartCSS.cartDropDown}
-          style={{ display: this.state.isOpen ? "block" : "none" }}
-        >
-          <ul>
-            <li>Napoletana</li>
-            <li>Marinara</li>
-          </ul>
-        </div>
-      </div>
+      <AppStateContext.Consumer>
+        {(state) => {
+          return (
+            <div className={CartCSS.cartContainer}>
+              <button
+                type="button"
+                className={CartCSS.button}
+                onClick={this.handleClick}
+              >
+                <FiShoppingCart />
+                <span>{state.cart.items.length} pizza(s)</span>
+              </button>
+              <div
+                className={CartCSS.cartDropDown}
+                style={{ display: this.state.isOpen ? "block" : "none" }}
+              >
+                <ul>
+                  {state.cart.items.map((item) => (
+                    <li key={item.id}>
+                      {item.name} &times; {item.quantity}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          );
+        }}
+      </AppStateContext.Consumer>
     );
   }
 }
